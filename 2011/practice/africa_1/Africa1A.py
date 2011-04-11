40576c4494b583f37cd8a4593d12ca0feedb4d5c
Mark Smith <mark.smith@practicalpoetry.co.uk>
2011-04-11 17:56:01 +0100

    Case functions now take messages rather than instance vars. Moved solution to a shared dir.

--- 2011/practice/africa_1/Africa1A.py
+++ 2011/practice/africa_1/Africa1A.py
@@ -1,34 +1,37 @@
+import sys
+sys.path[0:0] = ['..']
+
 import solution
 
 
 class Africa1A(solution.Case):
-	def read_case(self):
-		self.c = self.read_int()
-		self.i = self.read_int()
-		self.p_list = self.read_int_list()
-	
-	def solve_case(self):
-		p_len = len(self.p_list)
-		
-		indexes, self.p_list = zip(
-				*sorted(
-						zip(range(1, p_len+1), self.p_list),
-						key=lambda x: x[1]))
-		
-		for i in range(p_len):
-			val1 = self.p_list[i]
-			for j in range(i+1, p_len):
-				res = val1 + self.p_list[j]
-				if res > self.c:
-					break
-				elif res == self.c:
-					self.result = sorted([indexes[i], indexes[j]])
-					return
-		raise Exception()
-	
-	def write_case(self):
-		self.write_int_list(self.result)
+    def read_case(self):
+        c = self.read_int()
+        i = self.read_int()
+        p_list = self.read_int_list()
+        return c, i, p_list
+    
+    def solve_case(self, c, i, p_list):
+        p_len = len(p_list)
+        
+        indexes, p_list = zip(
+                *sorted(
+                        zip(range(1, p_len+1), p_list),
+                        key=lambda x: x[1]))
+        
+        for i in range(p_len):
+            val1 = p_list[i]
+            for j in range(i+1, p_len):
+                res = val1 + p_list[j]
+                if res > c:
+                    break
+                elif res == c:
+                    return sorted([indexes[i], indexes[j]])
+        raise Exception()
+    
+    def write_case(self, result):
+        self.write_int_list(result)
 
 
 if __name__ == '__main__':
-	solution.main(Africa1A)
+    solution.main(Africa1A)
--- 2011/practice/africa_1/Africa1B.py
+++ 2011/practice/africa_1/Africa1B.py
@@ -1,15 +1,18 @@
+import sys
+sys.path[0:0] = ['..']
+
 import solution
 
 class Africa1B(solution.Case):
-	def read_case(self):
-		self.line = self.fin.readline().strip()
-	
-	def solve_case(self):
-		self.result = ' '.join(reversed(self.line.split(' ')))
-	
-	def write_case(self):
-		print >>self.fout, self.result
+    def read_case(self):
+        return self.fin.readline().strip(),
+    
+    def solve_case(self, line):
+        return ' '.join(reversed(line.split(' ')))
+    
+    def write_case(self, result):
+        print >>self.fout, result
 
 
 if __name__ == '__main__':
-	solution.main(Africa1B)
+    solution.main(Africa1B)
