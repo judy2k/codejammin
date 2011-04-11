f9e27d6887b8318655bdee5c704155d5018453b5
Mark Smith <mark.smith@practicalpoetry.co.uk>
2011-04-11 14:41:39 +0100

    Added a skeleton file for Africa 2011 1B

---
+++ 2011/practice/africa_1_a/Africa1B.py
@@ -0,0 +1,15 @@
+import solution
+
+class Africa1B(solution.Case):
+	def read_case(self):
+		pass
+	
+	def solve_case(self):
+		pass
+	
+	def write_case(self):
+		pass
+
+
+if __name__ == '__main__':
+	solution.main(Africa1B)
